6987be0b20cbf059185259171fb974c859d2fccc
bnznamco <gabriele.baldi.01@gmail.com>
2024-07-08 11:39:52 +0200

    feat: update model search endpoint to admit direct pk searches

--- structured/urls.py
+++ structured/urls.py
@@ -2,5 +2,5 @@ from structured.views import search
 from django.urls import path
 
 urlpatterns = [
-    path("structured_field/search_model/", search, name="search"),
+    path("structured_field/search_model/<str:model>/", search, name="search"),
 ]
--- structured/views.py
+++ structured/views.py
@@ -4,19 +4,23 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 
 @staff_member_required
-def search(request):
+def search(request, model):
     if request.method == "GET":
-        model = request.GET.get("_m", None)
         search_term = request.GET.get("_q", None)
-        if model and search_term:
+        if search_term:
             model = apps.get_model(*model.split("."))
             if model:
                 if search_term == "__all__":
                     results = model.objects.all()
+                if search_term.startswith("_pk="):
+                    results = model.objects.filter(pk=search_term.split("_pk=")[1])
+                if search_term.startswith("_pk__in="):
+                    pks = search_term.split("_pk__in=")[1].split(",")
+                    results = model.objects.filter(pk__in=pks)
                 else:
                     results = model.objects.filter(name__icontains=search_term)[:20]
                 json_results = [
-                    {"id": result.pk, "name": result.__str__()} for result in results
+                    {"id": result.pk, "name": result.__str__() or f"{model.__name__} ({result.pk})"} for result in results
                 ]
                 return JsonResponse(json_results, safe=False)
     return JsonResponse([], safe=False)
