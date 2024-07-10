63277ea75d0dea85aeca845f25d658c59b4db2e0
bnznamco <gabriele.baldi.01@gmail.com>
2024-07-10 17:43:12 +0200

    tests: 85% coverage reached :thumbs_up: 👍

--- tests/test_admin_editor.py
+++ tests/test_admin_editor.py
@@ -1,4 +1,5 @@
 import pytest
+from tests.app.test_module.models import SimpleRelationModel
 
 
 # Django admin custom widget is rendered correctly
@@ -16,3 +17,124 @@ def test_admin_custom_widget(admin_client):
     ]
     for resource in resources:
         assert resource in str(response.content)
+
+
+# Django admin custom widget can create simple data (name, age fields)
+@pytest.mark.django_db
+def test_admin_custom_widget_create_simple_data(admin_client):
+    response = admin_client.post(
+        "/admin/test_module/testmodel/add/",
+        {
+            "title": "Content",
+            "structured_data": '{"name": "John Doe", "age": 30}',
+        },
+    )
+    assert response.status_code == 302
+    assert response.url == "/admin/test_module/testmodel/"
+    response = admin_client.get("/admin/test_module/testmodel/1/change/")
+    assert response.status_code == 200
+    assert "John Doe" in str(response.content)
+    assert "30" in str(response.content)
+
+
+# Django admin custom widget can create nested data (name, age, child fields)
+@pytest.mark.django_db
+def test_admin_custom_widget_create_nested_data(admin_client):
+    response = admin_client.post(
+        "/admin/test_module/testmodel/add/",
+        {
+            "title": "Content",
+            "structured_data": '{"name": "John Doe", "age": 30, "child": {"name": "Jane Doe", "age": 25}}',
+        },
+    )
+    assert response.status_code == 302
+    assert response.url == "/admin/test_module/testmodel/"
+    response = admin_client.get("/admin/test_module/testmodel/1/change/")
+    assert response.status_code == 200
+    assert "John Doe" in str(response.content)
+    assert "30" in str(response.content)
+    assert "Jane Doe" in str(response.content)
+    assert "25" in str(response.content)
+
+
+# Django admin custom widget can create and then update data (name, age, child fields)
+@pytest.mark.django_db
+def test_admin_custom_widget_update_nested_data(admin_client):
+    response = admin_client.post(
+        "/admin/test_module/testmodel/add/",
+        {
+            "title": "Content",
+            "structured_data": '{"name": "John Doe", "age": 30, "child": {"name": "Jane Doe", "age": 25}}',
+        },
+    )
+    assert response.status_code == 302
+    assert response.url == "/admin/test_module/testmodel/"
+    response = admin_client.get("/admin/test_module/testmodel/1/change/")
+    assert response.status_code == 200
+    assert "John Doe" in str(response.content)
+    assert "30" in str(response.content)
+    assert "Jane Doe" in str(response.content)
+    assert "25" in str(response.content)
+    response = admin_client.post(
+        "/admin/test_module/testmodel/1/change/",
+        {
+            "title": "Content",
+            "structured_data": '{"name": "John Doe", "age": 30, "child": {"name": "Jane Doe", "age": 26}}',
+        },
+    )
+    assert response.status_code == 302
+    assert response.url == "/admin/test_module/testmodel/"
+    response = admin_client.get("/admin/test_module/testmodel/1/change/")
+    assert response.status_code == 200
+    assert "John Doe" in str(response.content)
+    assert "30" in str(response.content)
+    assert "Jane Doe" in str(response.content)
+    assert "26" in str(response.content)
+
+
+# Django admin custom widget can create fk and qs fields
+@pytest.mark.django_db
+def test_admin_custom_widget_create_fk_qs_fields(admin_client):
+    SimpleRelationModel.objects.bulk_create(
+        [SimpleRelationModel(name=name) for name in ["test1", "test2"]]
+    )
+    response = admin_client.post(
+        "/admin/test_module/testmodel/add/",
+        {
+            "title": "Content",
+            "structured_data": '{"name": "John Doe", "age": 30, "fk_field": 1, "qs_field": [1, 2]}',
+        },
+    )
+    assert response.status_code == 302
+    assert response.url == "/admin/test_module/testmodel/"
+    response = admin_client.get("/admin/test_module/testmodel/1/change/")
+    assert response.status_code == 200
+    assert "John Doe" in str(response.content)
+    assert "30" in str(response.content)
+    assert "test1" in str(response.content)
+    assert "test2" in str(response.content)
+
+
+# Django admin custom widget can create nested data with fk and qs fields
+@pytest.mark.django_db
+def test_admin_custom_widget_create_nested_fk_qs_fields(admin_client):
+    SimpleRelationModel.objects.bulk_create(
+        [SimpleRelationModel(name=name) for name in ["test1", "test2"]]
+    )
+    response = admin_client.post(
+        "/admin/test_module/testmodel/add/",
+        {
+            "title": "Content",
+            "structured_data": '{"name": "John Doe", "age": 30, "child": {"name": "Jane Doe", "age": 25, "fk_field": 1, "qs_field": [1, 2]}}',
+        },
+    )
+    assert response.status_code == 302
+    assert response.url == "/admin/test_module/testmodel/"
+    response = admin_client.get("/admin/test_module/testmodel/1/change/")
+    assert response.status_code == 200
+    assert "John Doe" in str(response.content)
+    assert "30" in str(response.content)
+    assert "Jane Doe" in str(response.content)
+    assert "25" in str(response.content)
+    assert "test1" in str(response.content)
+    assert "test2" in str(response.content)
---
+++ tests/test_admin_search.py
@@ -0,0 +1,67 @@
+import pytest, random
+from tests.app.test_module.models import SimpleRelationModel
+
+
+# Test structured_field/search_model/<model> endpoint search
+@pytest.mark.django_db
+def test_search_model(admin_client):
+    response = admin_client.get(
+        "/structured_field/search_model/test_module.SimpleRelationModel/"
+    )
+    body = response.json()
+    assert response.status_code == 200
+    assert body == []
+    names = ["test1", "test2", "test3", "test4", "test5"]
+    SimpleRelationModel.objects.bulk_create(
+        [SimpleRelationModel(name=name) for name in names]
+    )
+    response = admin_client.get(
+        "/structured_field/search_model/test_module.SimpleRelationModel/?_q=test"
+    )
+    body = response.json()
+    assert response.status_code == 200
+    assert len(body) == 5
+
+
+# Test structured_field/search_model/<model> endpoint search with _pk
+@pytest.mark.django_db
+def test_search_model_pk(admin_client):
+    names = ["test1", "test2", "test3", "test4", "test5"]
+    SimpleRelationModel.objects.bulk_create(
+        [SimpleRelationModel(name=name) for name in names]
+    )
+    model_list = list(SimpleRelationModel.objects.all())
+
+    index = random.randint(0, len(model_list) - 1)
+
+    response = admin_client.get(
+        f"/structured_field/search_model/test_module.SimpleRelationModel/?_q=_pk={model_list[index].pk}"
+    )
+    body = response.json()
+    assert response.status_code == 200
+    assert len(body) == 1
+    assert body[0]["id"] == model_list[index].pk
+    assert body[0]["name"] == model_list[index].name
+
+
+# Test structured_field/search_model/<model> endpoint search with _pk__in
+@pytest.mark.django_db
+def test_search_model_pks(admin_client):
+    names = ["test1", "test2", "test3", "test4", "test5"]
+    SimpleRelationModel.objects.bulk_create(
+        [SimpleRelationModel(name=name) for name in names]
+    )
+    model_list = list(SimpleRelationModel.objects.all())
+
+    random_indexes = random.sample(range(len(model_list)), 2)   
+
+    response = admin_client.get(
+        f"/structured_field/search_model/test_module.SimpleRelationModel/?_q=_pk__in={','.join([str(model_list[i].pk) for i in random_indexes])}"
+    )
+    body = response.json()
+    assert response.status_code == 200
+    assert len(body) == 2
+    assert model_list[random_indexes[0]].pk in [item["id"] for item in body]
+    assert model_list[random_indexes[1]].pk in [item["id"] for item in body]
+    assert model_list[random_indexes[0]].name in [item["name"] for item in body]
+    assert model_list[random_indexes[1]].name in [item["name"] for item in body]
