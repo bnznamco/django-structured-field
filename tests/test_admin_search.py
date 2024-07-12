42a90218fddb2374e13dc024389136d7afa7cba2
bnznamco <gabriele.baldi.01@gmail.com>
2024-07-12 10:18:59 +0200

    tests: more tests

--- tests/test_admin_search.py
+++ tests/test_admin_search.py
@@ -67,3 +67,56 @@ def test_search_model_pks(admin_client):
     assert model_list[random_indexes[1]].pk in [item["id"] for item in body]
     assert model_list[random_indexes[0]].name in [item["name"] for item in body]
     assert model_list[random_indexes[1]].name in [item["name"] for item in body]
+    
+    
+# Test structured_field/search_model/<model> with __all__ search
+@pytest.mark.django_db
+def test_search_model_all(admin_client):
+    from tests.app.test_module.models import SimpleRelationModel
+    names = ["test1", "test2", "test3", "test4", "test5"]
+    SimpleRelationModel.objects.bulk_create(
+        [SimpleRelationModel(name=name) for name in names]
+    )
+    response = admin_client.get(
+        f"/structured_field/search_model/test_module.SimpleRelationModel/?_q=__all__"
+    )
+    body = response.json()
+    assert response.status_code == 200
+    assert len(body) == 5
+    assert all([item["name"] in names for item in body])
+    assert all([item["id"] in [model.pk for model in SimpleRelationModel.objects.all()] for item in body])
+
+
+# Test structured_field/search_model/<model> with wrong method
+@pytest.mark.django_db
+def test_search_model_wrong_method(admin_client):
+    response = admin_client.post(
+        "/structured_field/search_model/test_module.SimpleRelationModel/"
+    )
+    body = response.json()
+    assert response.status_code == 405
+    assert body["error"] == "Method Not Allowed"
+    
+# Test structured_field/search_model/<model> with wrong model
+@pytest.mark.django_db
+def test_search_model_wrong_model(admin_client):
+    response = admin_client.get(
+        "/structured_field/search_model/test_module.SimpleRelationModelWrong/"
+    )
+    assert response.status_code == 404
+    
+# Test not digit pk
+@pytest.mark.django_db
+def test_search_model_not_digit_pk(admin_client):
+    from tests.app.test_module.models import SimpleRelationModel
+    names = ["test1", "test2", "test3", "test4", "test5"]
+    SimpleRelationModel.objects.bulk_create(
+        [SimpleRelationModel(name=name) for name in names]
+    )
+    response = admin_client.get(
+        f"/structured_field/search_model/test_module.SimpleRelationModel/?_q=_pk=not_digit"
+    )
+    body = response.json()
+    assert response.status_code == 200
+    assert len(body) == 0
+    assert body == []
