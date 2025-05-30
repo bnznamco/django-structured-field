d5d50675d831b2a9281714de302431e5355b7979
bnznamco <gabriele.baldi.01@gmail.com>
2025-05-30 10:07:16 +0200

    chore: added some logging to the admin widget to better debug admin panel errors

--- structured/widget/fields.py
+++ structured/widget/fields.py
@@ -1,17 +1,23 @@
 from typing import Any, Union
 from structured.pydantic.models import BaseModel
 from structured.widget.widgets import StructuredJSONFormWidget
-from django.forms import JSONField, ValidationError
 from pydantic import ValidationError as PydanticValidationError
+from django.forms import JSONField, ValidationError
 from django.utils.translation import gettext_lazy as _
 import json
 import traceback
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class StructuredJSONFormField(JSONField):
     widget = StructuredJSONFormWidget
     default_error_messages = {
         "invalid": _("Check errors below."),
+        "invalid_schema": _(
+            "The provided data does not match the expected schema. Please check the errors below."
+        ),
     }
 
     def __init__(self, schema, ui_schema=None, *args, **kwargs):
@@ -25,7 +31,15 @@ class StructuredJSONFormField(JSONField):
             return value and self.schema.validate_python(value.copy())
         except PydanticValidationError:
             traceback.print_exc()
-            raise ValidationError(self.error_messages["invalid"], code="invalid")
+            logger.error(
+                "Validation error in StructuredJSONFormField",
+                extra={
+                    "value": value,
+                    "schema": self.schema,
+                    "ui_schema": self.ui_schema,
+                },
+            )
+            raise ValidationError(self.error_messages["invalid_schema"], code="invalid_schema")
 
     def to_python(self, value: Any) -> Any:
         value = super().to_python(value)
